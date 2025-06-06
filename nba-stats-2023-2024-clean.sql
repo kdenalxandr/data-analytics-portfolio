84891783e8ac0a08ba84d6c3a7f5f149440c0398
kdenalxandr <113318742+kdenalxandr@users.noreply.github.com>
2025-06-06 18:29:31 -0500

    Update nba-stats-2023-2024-clean.sql
    added team performance ranking note

--- nba-stats-2023-2024-clean.sql
+++ nba-stats-2023-2024-clean.sql
@@ -1,4 +1,5 @@
 --Filtering out the last league totals row from basketball-referenece
+--Showcases Team Performance Ranking
 CREATE Table `vernal-reality-440716-s9.nba_analysis.2023_2024_nba_stats_clean` AS 
 SELECT * 
 FROM `vernal-reality-440716-s9.nba_analysis.2023_2024_nba_stats` 
