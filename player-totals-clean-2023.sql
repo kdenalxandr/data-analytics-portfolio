fa76912006271c944906522d1f2f6bf934982b92
kdenalxandr <113318742+kdenalxandr@users.noreply.github.com>
2025-06-06 18:30:30 -0500

    Update player-totals-clean-2023.sql

--- player-totals-clean-2023.sql
+++ player-totals-clean-2023.sql
@@ -1,4 +1,4 @@
---Creating a table that counts how many players show up on more than one team
+--Creating a table that counts how many players show up on more than one team (creating duplicate rows)
 CREATE TABLE `vernal-reality-440716-s9.nba_analysis.player_totals_23_clean` AS
 WITH PlayerCounts AS(
   SELECT Player,
@@ -6,7 +6,7 @@ WITH PlayerCounts AS(
   FROM `vernal-reality-440716-s9.nba_analysis.player_totals_23`
   GROUP BY Player
 )
---Joining keeping the total stats 2TM row and ordering the table by player rank
+--Joining tables in order to keep the total stats (2TM row) and ordering the table by player rank
 SELECT a.*
 FROM `vernal-reality-440716-s9.nba_analysis.player_totals_23` a
 JOIN PlayerCounts b ON a.Player = b.Player
