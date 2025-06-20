0d89206d04234f33d5710343ad8ca8a6b51ebf8e
kdenalxandr <113318742+kdenalxandr@users.noreply.github.com>
2025-06-20 18:10:39 -0500

    Update avg-basketball-trb.sql

--- avg-basketball-trb.sql
+++ avg-basketball-trb.sql
@@ -1,7 +1,7 @@
 -- Comparing Total Rebound Percent Change for teams from the 2023 and 2024 season.
 SELECT a.Team,
-ROUND(a.TRB, 2) AS avg_trb_24,
-ROUND(b.TRB, 2) AS avg_trb_23,
+a.TRB AS avg_trb_24,
+b.TRB AS avg_trb_23,
 ROUND(a.TRB - b.TRB, 2) as TRB_change
 FROM `vernal-reality-440716-s9.nba_analysis.2023_2024_nba_stats_clean` a
 JOIN `vernal-reality-440716-s9.nba_analysis.2022_2023_nba_stats_clean` b
